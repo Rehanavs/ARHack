7fb4dd5549eecca35ed44e5f2b4bb3f11c638370
Zeenat Karolia <zeenat.karolia@standardbank.co.za>
2019-07-22 14:26:04 +0200

    added custom elements schema

--- ARHack-Clientside/src/app/app.module.ts
+++ ARHack-Clientside/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
@@ -18,6 +18,7 @@ import { SigninComponent } from './components/signin/signin.component';
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
