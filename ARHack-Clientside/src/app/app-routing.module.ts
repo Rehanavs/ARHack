1e5fd77fea004207cdbffc0c11ed416716c5ff3d
Rehana van Schalkwyk <riaana.vanSchalkwyk@standardbank.co.za>
2019-07-22 14:00:37 +0200

    Added routing for signIn and copied signIn background

--- ARHack-Clientside/src/app/app-routing.module.ts
+++ ARHack-Clientside/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SigninComponent} from './components/signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'signIn', pathMatch: 'full'},
+  {path: 'signIn', component: SigninComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+// export const AppRoutingModule = RouterModule.forRoot(routes);
+
